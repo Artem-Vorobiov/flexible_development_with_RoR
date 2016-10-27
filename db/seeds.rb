910a931aa0e62fc84c901c27d52376537cc3eff8
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-10-27 21:41:58 +0300

    AJAX

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.delete_all
+#Product.delete_all
 # . . .
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-description:
-%{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-image_url: 'http://www.zastavki.com/pictures/640x480/2015/World___USA_Mount_McKinley__Alaska_096388_29.jpg',
-price: 49.95)
+
+Product.create!(title: 'Text 2',
+  description:
+    %{<p>
+      Lorem ipsum dolor sit amet.
+    </p>},
+  image_url: 'https://ssl.gstatic.com/gb/images/v1_76783e20.png',
+  price: 2.95)
+# . . .
+Product.create!(title: 'Text 3',
+  description:
+    %{<p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perferendis iusto sapiente nulla commodi voluptates fugiat, a consectetur nesciunt error?
+    </p>},
+  image_url: 'https://lh3.googleusercontent.com/-rKCSApUiydQ/AAAAAAAAAAI/AAAAAAAAAAA/APaXHhRrdrZpgZabnZe0crFGsiWn3CQJRw/s96-c-mo/photo.jpg',
+  price: 3.95)
+# . . .
+Product.create!(title: 'Text 4',
+  description:
+    %{<p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo optio illo debitis animi enim sint dolore temporibus, autem in, totam labore saepe adipisci qui aspernatur.
+    </p>},
+  image_url: 'https://www.google.com.ua/images/kg/social/gplus_32x32.png',
+  price: 4.95)
 # . . .
