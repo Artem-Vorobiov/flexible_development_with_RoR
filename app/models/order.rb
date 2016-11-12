1779b9fb3fce89f6776ff65f13fef25bcc2f4911
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-11-12 19:02:01 +0300

    Fixed troubles with address(in Order.rb - forgot 's' in word address) & fixed display my temporaly cart  added 'optional: true' in line_item.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
 
   PAYMENT_TYPES = ["Check", "Credit Card", "Purchase order"]
 
-  validates :name, :addres, :email, presence: true
+  validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
 
   def add_line_items_from_cart(cart)
